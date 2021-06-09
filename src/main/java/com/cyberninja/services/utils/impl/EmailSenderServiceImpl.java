53c294c5095ae546973d4f62a3184255cb19d2bc
WaterLord404 <55837653+WaterLord404@users.noreply.github.com>
2021-06-09 20:36:44 +0200

    domain front constant, max size images 10mb

--- src/main/java/com/cyberninja/services/utils/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/cyberninja/services/utils/impl/EmailSenderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cyberninja.services.utils.impl;
 
+import static com.cyberninja.common.ApplicationConstans.FRONT_DOMAIN;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,8 +13,9 @@ import com.cyberninja.services.utils.EmailSenderServiceI;
 @Service
 public class EmailSenderServiceImpl implements EmailSenderServiceI {
 
-	@Autowired private JavaMailSender javaMailSender;
-	
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	/**
 	 * Envia el email
 	 */
@@ -20,13 +23,13 @@ public class EmailSenderServiceImpl implements EmailSenderServiceI {
 	@Async
 	public void sendEmail(String email, String token) {
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
-		
+
 		mailMessage.setTo(email);
-        mailMessage.setSubject("Complete Registration!");
-        mailMessage.setFrom("cyberninjaes@gmail.com");
-        mailMessage.setText("To confirm your account, please click here: " + 
-        "http://localhost:4200/account/confirm-account/" + token);
-		
+		mailMessage.setSubject("Complete Registration!");
+		mailMessage.setFrom("cyberninjaes@gmail.com");
+		mailMessage.setText("To confirm your account, please click here: " +
+				"http://" + FRONT_DOMAIN + "/account/confirm-account/" + token);
+
 		javaMailSender.send(mailMessage);
 	}
 }
