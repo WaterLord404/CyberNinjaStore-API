1a14f661edb47be063e96ac35b36fda190ba54cd
WaterLord404 <55837653+WaterLord404@users.noreply.github.com>
2021-06-12 01:06:12 +0200

    email send html

--- src/main/java/com/cyberninja/services/utils/EmailSenderServiceI.java
+++ src/main/java/com/cyberninja/services/utils/EmailSenderServiceI.java
@@ -1,7 +1,9 @@
 package com.cyberninja.services.utils;
 
+import javax.mail.MessagingException;
+
 public interface EmailSenderServiceI {
 
-	void sendEmail(String email, String token);
+	void sendEmail(String email, String token) throws MessagingException;
 
 }
